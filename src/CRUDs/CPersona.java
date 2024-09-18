72ff20732ff89f26438acc7f13e57827a9682166
Al200518202419 <alonzomorales049@gmail.com>
2024-09-17 23:40:10 -0600

    Avance de CPersona

--- src/CRUDs/CPersona.java
+++ src/CRUDs/CPersona.java
@@ -28,8 +28,15 @@ public class CPersona {
             criteria.setProjection(Projections.projectionList()
                     .add(Projections.property("idPersona"))
                     .add(Projections.property("cui"))
+                    .add(Projections.property("codigoPersonal"))
                     .add(Projections.property("nombre"))
                     .add(Projections.property("apellido"))
+                    .add(Projections.property("fechaNacimiento"))
+                    .add(Projections.property("nacionalidad"))
+                    .add(Projections.property("genero"))
+                    .add(Projections.property("rol"))
+                    .add(Projections.property("codigoEscuela"))
+                      
             );
 
         } catch (Exception e) {
@@ -40,22 +47,27 @@ public class CPersona {
         return lista;
     }
 
-    public static boolean crear(String codigoEscuela, String nombre, String direccion, String telefono) {
+    public static boolean crear(String cui, String codigoPersonal, String nombre, String apellido, String fechaNacimiento, String nacionalidad, String genero, String rol, String codigoEscuela) {
         boolean flag = false;
         Session session = HibernateUtil.HibernateUtil.getSessionFactory().openSession();
-        Criteria criteria = session.createCriteria(Escuela.class);
-        criteria.add(Restrictions.eq("codigoEscuela", codigoEscuela));
+        Criteria criteria = session.createCriteria(Persona.class);
+        criteria.add(Restrictions.eq("cui", cui));
         criteria.add(Restrictions.eq("borradoLogico", true));
-        Escuela insert = (Escuela) criteria.uniqueResult();
+        Persona insert = (Persona) criteria.uniqueResult();
         Transaction transaction = null;
         try {
             transaction = session.beginTransaction();
             if (insert == null) {
-                insert = new Escuela();
-                insert.setCodigoEscuela(codigoEscuela);
+                insert = new Persona();
+                insert.setCui(cui);
+                insert.setCodigoPersonal(codigoPersonal);
                 insert.setNombre(nombre);
-                insert.setDireccion(direccion);
-                insert.setTelefono(telefono);
+                insert.setApellido(apellido);
+                insert.setFechaNacimiento(fechaNacimiento);
+                insert.setNacionalidad(nacionalidad);
+                insert.setGenero(genero);
+                insert.setRol(rol);
+                insert.setCodigoEscuela(codigoEscuela);
                 insert.setBorradoLogico(true);
                 session.save(insert);
                 flag = true;
@@ -69,20 +81,26 @@ public class CPersona {
         return flag;
     }
     // este es nuestro metodo actualizar, 
-        public static boolean actualizar(String codigoEscuela, String nombre, String direccion, String telefono) {
+       public static boolean actualizar(int idPersona, String cui, String codigoPersonal, String nombre, String apellido, String fechaNacimiento, String nacionalidad, String genero, String rol, String codigoEscuela) {
         boolean flag = false;
         Session session = HibernateUtil.HibernateUtil.getSessionFactory().openSession();
-        Criteria criteria = session.createCriteria(Escuela.class);
-        criteria.add(Restrictions.eq("codigoEscuela", codigoEscuela)); // corrigir datos escuela
-        Escuela insert = (Escuela) criteria.uniqueResult(); // obtener la persona existente 
+        Criteria criteria = session.createCriteria(Persona.class);
+        criteria.add(Restrictions.eq("idPersona", idPersona)); // corrigir datos escuela
+        Persona insert = (Persona) criteria.uniqueResult(); // obtener la persona existente 
         Transaction transaction = null;
         try {
             transaction = session.beginTransaction();
             if (insert != null) {
+                insert.setCui(cui);
+                insert.setCodigoPersonal(codigoPersonal);
                 insert.setNombre(nombre);
-                insert.setDireccion(direccion);
-                insert.setTelefono(telefono);
-                session.update(insert);
+                insert.setApellido(apellido);
+                insert.setFechaNacimiento(fechaNacimiento);
+                insert.setNacionalidad(nacionalidad);
+                insert.setGenero(genero);
+                insert.setRol(rol);
+                insert.setcodigoEscuela(codigoEscuela);
+                insert.setBorradoLogico(true);
                 flag = true;
             }
             transaction.commit();
